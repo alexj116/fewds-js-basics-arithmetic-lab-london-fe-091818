ccf408748b6f90e9ca80f8283ffbd7a1274050f1
Alexandra Johnson <alexandra@queryclick.com>
2018-10-23 15:39:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const newID=oldID+1000000000;
 
 //AgeIsValid contains True when the value in currentAge is a valid inteher
 
+ let currentAge = "ageNow";
+integer(`Wake ${dogName} the ${dogBreed}`);
+
+
 const randomNumber=Math.floor((Math.random() * 20) + 1);
 
 const randomInteger=Math.round(randomNumber);
