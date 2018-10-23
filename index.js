fc1f3feaed9837ec652332b6b4d0e9e9a9d22aea
Alexandra Johnson <alexandra@queryclick.com>
2018-10-23 15:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+const newID=oldID+1000000000;
+
+//AgeIsValid contains True when the value in currentAge is a valid inteher
+
+const randomNumber=Math.floor((Math.random() * 20) + 1);
+
+const randomInteger=Math.round(randomNumber);
+
+const randomUserID=randomInteger + 1000000000
